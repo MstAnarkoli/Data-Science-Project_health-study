6f017041320ff94cf24d4ed8ac2ab9e21787d6fd
Mst Anarkoli <anarkoliniru@gmail.com>
2025-11-28 00:18:16 +0600

    PCA analysis

--- src/hälsostudie.py
+++ src/hälsostudie.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from metrics import regression_bp_age
+#from metrics import regression_bp_age
 #from viz import 
 
 
